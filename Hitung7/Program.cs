4c92925731df51e52cbc2512246245415e605330
mohammad-imron <ronnie_plsr@yahoo.com>
2023-05-28 00:17:15 +0700

    Hitung tujuh delapan sembilan

---
+++ Hitung7/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//int firstnumber;
+//int secondnumber;
+try
+{
+Console.Write("Input the First number : ");
+int firstnumber = Convert.ToInt32(Console.ReadLine()) ;
+Console.Write("Input the Second number : ");
+int secondnumber = Convert.ToInt32(Console.ReadLine()) ;
+Console.Write("Input the Third number : ");
+int thirdnumber = Convert.ToInt32(Console.ReadLine()) ;
+var result = firstnumber * secondnumber * thirdnumber ;
+Console.WriteLine($"{firstnumber} x {secondnumber} x {thirdnumber} = {result} ");
+}
+catch (Exception e)
+{
+Console.WriteLine(e.Message);
+}
---
+++ Hitung9/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+   int number1, number2, number3;
+ 
+    Console.Write("Enter first number - ");
+    number1 = Convert.ToInt32(Console.ReadLine());
+     
+    Console.Write("Enter second number - ");
+    number2 = Convert.ToInt32(Console.ReadLine());
+ 
+    Console.Write("Enter third number - ");
+    number3 = Convert.ToInt32(Console.ReadLine());
+ 
+    Console.Write("Result of specified numbers {0}, {1} and {2}, (x+y)·z is {3} and x*y + y*z is {4}\n\n",
+        number1, number2, number3, ((number1+number2)*number3), (number1*number2+number2*number3));
